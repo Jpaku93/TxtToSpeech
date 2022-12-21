1217eba74577ced3897b36782bd4b1e7defd5416
Jpaku93 <j.r.paku@hotmail.co.nz>
2022-12-21 21:13:07 +1300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-import pyttsx3
+# import pyttsx3
 import streamlit as st
 
 # Initialize text-to-speech engine
-speaker = pyttsx3.init()
+# speaker = pyttsx3.init()
 
 # Create a text area where the user can input the text to be converted
 text = st.text_area("Enter the text to be converted to an audio file:", height=400)
 
 # Create a button that the user can click to convert the text to an audio file
-if st.button("Convert to MP3"):
+# if st.button("Convert to MP3"):
     # Save the text as an MP3 audio file
-    speaker.save_to_file(text, "converted_text.mp3")
+    # speaker.save_to_file(text, "converted_text.mp3")
     # Play the audio file
-    speaker.runAndWait()
+    # speaker.runAndWait()
     # Stop the audio engine
-    speaker.stop()
-    st.audio("converted_text.mp3")
+    # speaker.stop()
+    # st.audio("converted_text.mp3")
     
