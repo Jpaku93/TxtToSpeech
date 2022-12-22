060729676200f36d901f3d87d31a42003436e7cd
Jpaku93 <j.r.paku@hotmail.co.nz>
2022-12-22 21:13:58 +1300

    Doesnt work on st

--- pyttsx.py
+++ pyttsx.py
@@ -1,21 +1,13 @@
 import pyttsx3
 import streamlit as st
 
-SPEAKER_LIST = {
-    'Voice 1 (Female)': 'en_0',
-    'Voice 2 (Male)': 'en_29',
-    'Voice 3 (Female)': 'en_41',
-    'Voice 4 (Male)': 'en_110'
-}
-
 # Initialize text-to-speech engine
 speaker = pyttsx3.init()
 # change the speaker's voice
 speaker.setProperty('voice', 'english+f3')
 # Create a text area where the user can input the text to be converted
 text = st.text_area("Enter the text to be converted to an audio file:", height=400)
-st.header('2. Please select voice')
-speaker = st.radio('Available voices:', SPEAKER_LIST.keys(), horizontal=True)
+
 # Create a button that the user can click to convert the text to an audio file
 if st.button("Convert to MP3"):
     # Save the text as an MP3 audio file
